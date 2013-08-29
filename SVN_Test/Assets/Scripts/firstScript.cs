9e01ed768b2e7d96b326ea7e4268678445ccac0b
elseven <elliott2@uga.edu>
2013-08-29 09:54:10 -0400

    With_Script
    added a script

---
+++ SVN_Test/Assets/Scripts/firstScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class firstScript : MonoBehaviour {
+	
+	public int a = 10;
+	public int b = 20;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
